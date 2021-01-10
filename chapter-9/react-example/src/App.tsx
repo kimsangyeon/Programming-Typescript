feb3b5b78a194f65f4851c0c61a0dc6f2f927c56
Kim Sangyeon <dho113@naver.com>
2021-01-10 10:18:59 +0900

    chapter-9

---
+++ chapter-9/react-example/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SignupForm } from './component/SignupForm'
+
+function App() {
+  return (
+    <div className="App">
+      <SignupForm firstName='Albert' userId='13abcd' />
+    </div>
+  );
+}
+
+export default App;
